302b3048bea8b30cb2243146cc02ce341999e1ce
Goncalo Almeida <goncalo.almeida88@gmail.com>
2024-08-08 15:29:42 +0100

    Initial Commit

---
+++ CurrencyConvApp/CurrencyConvAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConvAppApp.swift
+//  CurrencyConvApp
+//
+//  Created by GoncaloSAlmeida on 08/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConvAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
